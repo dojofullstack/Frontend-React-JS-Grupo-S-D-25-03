6e0379399638e20dc197fceb8e8d8fb65fb18708
dojofullstack <hola@dojofullstack.com>
2024-07-07 16:40:36 -0500

    first commit

---
+++ project-con-vite-recomendado/app-blog/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <>
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita nam ut aperiam?</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <img src="" alt="" />
+    </>
+  )
+}
+
+export default App;
